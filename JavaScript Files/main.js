451836f9105ac1c02ca7cc40688d05915dcf22c0
Srikar Lanka <122473218+slanka22@users.noreply.github.com>
2025-11-13 17:42:27 -0500

    Latest Updates from Thursday Coding Update
    Co-authored-by: angisardat <angisardat@users.noreply.github.com>
    Co-authored-by: Jan, Tyler Renze <trj5230@psu.edu>

--- JavaScript Files/main.js
+++ JavaScript Files/main.js
@@ -22,16 +22,11 @@ function loadRecipes(){
         <div class="card">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBC7QuVp-W5YBTP3qWiZIuSYFy8zkQFmLk8hBOvGhDG8UxuqyMilIJcuuQLhC151Qph0ISV7DKJ22xyW4XmSkBZ1lMTp9ixhQqYLx-VqRu&s=10" alt="${recipeTitle}">
         <h3>${recipeTitle}</h3>
-        <p>Total Time: ${recipePrep + recipeCook} mins</p>
+        <p>Total Time: ${parseInt(recipePrep) + parseInt(recipeCook)} mins</p>
         <p>Servings: ${recipeServings} servings</p>
         <p>${recipeDescr}</p>
         </div>
         `
-
-        // card.addEventListener("click", function() {
-        //     sessionStorage.setItem("selectedRecipe", JSON.stringify(recipe));
-        //     window.location.href = "recipeView.html";
-        // });
 
         container.appendChild(card);
     });
